407f5d23b5a1888f7a028e88f0d4b5de0ac1bbbd
caddy2519 <bi_chorfi@esi.dz>
2015-03-06 21:43:35 +0100

    Add round center button and its animations

--- app/src/main/java/com/itech/dzservices/MainActivity.java
+++ app/src/main/java/com/itech/dzservices/MainActivity.java
@@ -1,10 +1,16 @@
 package com.itech.dzservices;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
-import android.view.animation.RotateAnimation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,17 +18,22 @@ public class MainActivity extends Activity implements CircleLayout.OnItemSelecte
         CircleLayout.OnItemClickListener, CircleLayout.OnRotationFinishedListener, CircleLayout.OnCenterClickListener {
 
     private TextView selectedTextView;
+    public static LinearLayout l;
+    private CircleLayout circleMenu;
+    private AnimationDrawable animHidden;
+    public static MainActivity ma;
+    private AnimationDrawable animShown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        ma = this;
         // Set content view by passed extra
 
         setContentView(R.layout.activity_main);
 
         // Set listeners
-        CircleLayout circleMenu = (CircleLayout) findViewById(R.id.main_circle_layout);
+        circleMenu = (CircleLayout) findViewById(R.id.main_circle_layout);
         circleMenu.setOnItemSelectedListener(this);
         circleMenu.setOnItemClickListener(this);
         circleMenu.setOnRotationFinishedListener(this);
@@ -31,6 +42,19 @@ public class MainActivity extends Activity implements CircleLayout.OnItemSelecte
         selectedTextView = (TextView) findViewById(R.id.main_selected_textView);
         selectedTextView.setText(((CircleImageView) circleMenu
                 .getSelectedItem()).getName());
+
+        l = (LinearLayout) findViewById(R.id.center);
+
+        animateCenterHiddenCreate();
+        animateCenterShownCreate();
+        animateCenterShownPlay();
+
+    //    circleMenu.setObjectsHidden(true);
+
+      //  circleMenu.groupeButtons(circleMenu.isObjectsHidden());
+
+        //     animateCenterPlay();
+
     }
 
     @Override
@@ -38,7 +62,7 @@ public class MainActivity extends Activity implements CircleLayout.OnItemSelecte
         selectedTextView.setText(name);
 
         switch (view.getId()) {
-            case R.id.main_calendar_image:
+         /*   case R.id.main_calendar_image:
                 // Handle calendar selection
                 break;
             case R.id.main_cloud_image:
@@ -55,7 +79,7 @@ public class MainActivity extends Activity implements CircleLayout.OnItemSelecte
                 break;
             case R.id.main_tap_image:
                 // Handle tap selection
-                break;
+                break;*/
         }
     }
 
@@ -66,7 +90,7 @@ public class MainActivity extends Activity implements CircleLayout.OnItemSelecte
                 Toast.LENGTH_SHORT).show();
 
         switch (view.getId()) {
-            case R.id.main_calendar_image:
+          /*  case R.id.main_calendar_image:
                 // Handle calendar click
                 break;
             case R.id.main_cloud_image:
@@ -84,20 +108,200 @@ public class MainActivity extends Activity implements CircleLayout.OnItemSelecte
             case R.id.main_tap_image:
                 // Handle tap click
                 break;
+
+          */
         }
     }
 
     @Override
     public void onRotationFinished(View view, String name) {
-        Animation animation = new RotateAnimation(0, 360, view.getWidth() / 2,
-                view.getHeight() / 2);
-        animation.setDuration(250);
+     /*   Animation animation = new RotateAnimation(0, 360, view.getWidth() / 2,
+                view.getHeight() / 2);*/
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.anim_scale);
+        animation.setDuration(240);
         view.startAnimation(animation);
     }
 
     @Override
     public void onCenterClick() {
-        Toast.makeText(getApplicationContext(), R.string.center_click,
-                Toast.LENGTH_SHORT).show();
+      /*  Toast.makeText(getApplicationContext(), R.string.center_click,
+                Toast.LENGTH_SHORT).show();*/
+
+        circleMenu.groupeButtons(circleMenu.isObjectsHidden());
     }
+
+    public void animateCenterHiddenPlay() {
+        l.setBackgroundDrawable(animHidden);
+        animHidden.run();
+
+    }
+
+    public void animateCenterHiddenStop() {
+
+        animHidden.stop();
+
+    }
+
+    public void animateCenterHiddenCreate() {
+        animHidden = new AnimationDrawable();
+
+        //Bitmap bm = decodeSampledBitmapFromUri();
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inScaled=true;
+        options.inPreferredConfig= Bitmap.Config.RGB_565;
+
+        //   options.inSampleSize=2;
+
+      //  options.inPurgeable = true;
+
+        animHidden.addFrame(  new BitmapDrawable(getResources(), BitmapFactory
+                .decodeResource(getResources(),R.drawable.back_moon0, options)), 40);
+        animHidden.addFrame(  new BitmapDrawable(getResources(), BitmapFactory
+                .decodeResource(getResources(),R.drawable.back_moon1, options)), 40);
+        animHidden.addFrame(  new BitmapDrawable(getResources(), BitmapFactory
+                .decodeResource(getResources(),R.drawable.back_moon2, options)), 40);
+        animHidden.addFrame(  new BitmapDrawable(getResources(), BitmapFactory
+                .decodeResource(getResources(),R.drawable.back_moon3, options)), 40);
+        animHidden.addFrame(  new BitmapDrawable(getResources(), BitmapFactory
+                .decodeResource(getResources(),R.drawable.back_moon4, options)), 40);
+        animHidden.addFrame(  new BitmapDrawable(getResources(), BitmapFactory
+                .decodeResource(getResources(),R.drawable.back_moon5, options)), 40);
+        animHidden.addFrame(  new BitmapDrawable(getResources(), BitmapFactory
+                .decodeResource(getResources(),R.drawable.back_moon6, options)), 40);
+        animHidden.addFrame(  new BitmapDrawable(getResources(), BitmapFactory
+                .decodeResource(getResources(),R.drawable.back_moon7, options)), 40);
+        animHidden.addFrame(  new BitmapDrawable(getResources(), BitmapFactory
+                .decodeResource(getResources(),R.drawable.back_moon8, options)), 40);
+        animHidden.addFrame(  new BitmapDrawable(getResources(), BitmapFactory
+                .decodeResource(getResources(),R.drawable.back_moon9, options)), 40);
+        animHidden.addFrame(  new BitmapDrawable(getResources(), BitmapFactory
+                .decodeResource(getResources(),R.drawable.back_moon10, options)), 40);
+        animHidden.addFrame(  new BitmapDrawable(getResources(), BitmapFactory
+                .decodeResource(getResources(),R.drawable.back_moon11, options)), 40);
+
+/*
+        animHidden.addFrame(
+                getResources().getDrawable(R.drawable.back_moon0),
+                40);
+        animHidden.addFrame(
+                getResources().getDrawable(R.drawable.back_moon1),
+                40);
+        animHidden.addFrame(
+                getResources().getDrawable(R.drawable.back_moon2),
+                40);
+        animHidden.addFrame(
+                getResources().getDrawable(R.drawable.back_moon3),
+                40);
+        animHidden.addFrame(
+                getResources().getDrawable(R.drawable.back_moon4),
+                40);
+        animHidden.addFrame(
+                getResources().getDrawable(R.drawable.back_moon5),
+                40);
+        animHidden.addFrame(
+                getResources().getDrawable(R.drawable.back_moon6),
+                40);
+        animHidden.addFrame(
+                getResources().getDrawable(R.drawable.back_moon7),
+                40);
+        animHidden.addFrame(
+                getResources().getDrawable(R.drawable.back_moon8),
+                40);
+        animHidden.addFrame(
+                getResources().getDrawable(R.drawable.back_moon9),
+                40);
+        animHidden.addFrame(
+                getResources().getDrawable(R.drawable.back_moon10),
+                40);
+        animHidden.addFrame(
+                getResources().getDrawable(R.drawable.back_moon11),
+                40);*/
+
+  /*      animHidden.addFrame(
+                getResources().getDrawable(R.drawable.back_moon12),
+                40);
+*/
+
+        //if you want the animation to loop, set false
+        animHidden.setOneShot(false);
+
+    }
+
+    public void animateCenterShownPlay() {
+        l.setBackgroundDrawable(animShown);
+        animShown.run();
+
+    }
+
+    public void animateCenterShownStop() {
+        animShown.stop();
+
+    }
+
+    public void animateCenterShownCreate() {
+
+        animShown = new AnimationDrawable();
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inPurgeable = true;
+
+        animShown.addFrame(  new BitmapDrawable(getResources(), BitmapFactory
+                .decodeResource(getResources(),R.drawable.back_moon_hidden0, options)), 600);
+        animShown.addFrame(  new BitmapDrawable(getResources(), BitmapFactory
+                .decodeResource(getResources(),R.drawable.back_moon_hidden1, options)), 600);
+/*
+        animShown.addFrame(
+                getResources().getDrawable(R.drawable.back_moon_hidden0),
+                600);
+        animShown.addFrame(
+                getResources().getDrawable(R.drawable.back_moon_hidden1),
+                600);
+*/
+        //if you want the animation to loop, set false
+        animShown.setOneShot(false);
+
+        animShown.run();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+     //   animShown = null;
+       // animHidden = null;
+   //     System.gc();
+        Log.d("Etat", "onStop");
+   //     invalidateOptionsMenu();
+     //   finish();
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+      //  Log.d("Etat", "onRestart");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+     //   Log.d("Etat", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //  Log.d("Etat", "onResume");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onRestart();
+      //  Log.d("Etat", "onDestroy");
+    }
+
+
 }
